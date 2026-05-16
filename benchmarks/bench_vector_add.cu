2f5de3a41f2e175cdfb0f1721e22bf37c5c3fb5b
Phil Tokamak <phil.tokamak@icloud.com>
2026-05-17 01:03:32 +0200

    Support auto-sweep parameters and write results to csv file

--- benchmarks/bench_vector_add.cu
+++ benchmarks/bench_vector_add.cu
@@ -3,6 +3,12 @@
 #include "bench_utils.hpp"
 
 #include <random>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <fmt/ranges.h>
+#include <fmt/ostream.h>
+#include <filesystem>
 #include <cuda/cmath>
 
 void init_array(float* a, size_t num_elem)
@@ -19,118 +25,250 @@ void init_array(float* a, size_t num_elem)
     }
 }
 
+template<typename T>
+inline size_t bytes_per_vector(size_t num_elem)
+{
+    return num_elem * sizeof(T);
+}
+
+inline size_t bytes_vector_add(size_t num_elem)
+{
+    // read a + read b + write c
+    return 3 * bytes_per_vector<float>(num_elem);
+}
+
+inline size_t h2d_bytes_vector_add(size_t num_elem)
+{
+    // copy a and b from host to device
+    return 2 * bytes_per_vector<float>(num_elem);
+}
+
+inline size_t d2h_bytes_vector_add(size_t num_elem)
+{
+    // copy c from deivec to host
+    return 1 * bytes_per_vector<float>(num_elem);
+}
+
+inline size_t num_flop_vector_add(size_t num_elem)
+{
+    // 1 addition per element
+    return bytes_per_vector<float>(num_elem);
+}
+
+void write_csv_header(std::ofstream& out)
+{
+    std::vector<std::string> headers
+    {
+        "kernel",    // kernel name, e.g. `vector_add`
+        "version",   // implementation version, e.g. `cpu_serial`, `cuda_naive`, `pageable memcpy`
+        "mode",      // measure mode, e.g. `cpu`, `cuda_kernel`, `h2d`, `d2h`
+        "dtype",     // data type, e.g. fp32
+        "n",         // vector length
+        "block_size",// CUDA block size (0 for CPU/H2D/D2H)
+        "repeat",    // number of measurement repetitions
+        "avg_ms",
+        "min_ms",
+        "max_ms",
+        "std_ms",
+        "bytes",     // bytes used for bandwidth calculation
+        "bw_GB_s",   // Effective bandwidth
+        "gflops",
+        "correct"
+    };
+
+    fmt::print(out, "{}\n", fmt::join(headers, ","));
+}
+
+void write_csv_row(std::ofstream& out,
+                   const std::string& kernel,
+                   const std::string& version,
+                   const std::string& mode,
+                   const std::string& dtype,
+                   size_t n,
+                   int block_size,
+                   int repeat,
+                   const TimeStats& time_stats,
+                   size_t bytes,
+                   bool correct)
+{
+    double bw_GB_s = bandwidth_GB_s(bytes, time_stats.avg_ms);
+    double gflops = calculate_gflops(num_flop_vector_add(n), time_stats.avg_ms);
+
+    fmt::print(out,
+               "{},{},{},{},{},{},{},{:.6f},{:.6f},{:.6f},{:3f},{},{:.3f},{:.3f},{}\n",
+               kernel,
+               version,
+               mode,
+               dtype,
+               n,
+               block_size,
+               repeat,
+               time_stats.avg_ms,
+               time_stats.min_ms,
+               time_stats.max_ms,
+               time_stats.std_ms,
+               bytes,
+               bw_GB_s,
+               gflops,
+               (correct ? "true" : "false"));
+}
+
 int main()
 {
-    size_t n = 1 << 26;
-
-    size_t bytes = n * sizeof(float);
-
-    fmt::print("Vector length n = {}\nArray size = {} MiB per vector\n", n, bytes/static_cast<double>(1ULL << 20));
-
-    // Initilize CUDA timer
+    // Initilize timers
     CudaTimer cuda_timer{CudaTimer()};
     CpuTimer cpu_timer{CpuTimer()};
 
-    // Pointers to host memory
-    host_ptr a_host(cuda_malloc_host<float>(n));
-    host_ptr b_host(cuda_malloc_host<float>(n));
-    host_ptr result_host(cuda_malloc_host<float>(n));
-    host_ptr result_from_dev(cuda_malloc_host<float>(n));
-
-    // Pointers to device memory
-    float* a_dev = nullptr;
-    float* b_dev = nullptr;
-    float* result_dev = nullptr;
-
-    // Initialize vectors on the host
-    init_array(a_host.get(), n);
-    init_array(b_host.get(), n);
-
-    // Allocate device memory
-    gpu::cuda_check(cudaMalloc(&a_dev, bytes));
-    gpu::cuda_check(cudaMalloc(&b_dev, bytes));
-    gpu::cuda_check(cudaMalloc(&result_dev, bytes));
-
-    cpu_timer.start();
-    // Copy data to the GPU
-    gpu::cuda_check(cudaMemcpy(a_dev, a_host.get(), bytes, cudaMemcpyDefault));
-    gpu::cuda_check(cudaMemcpy(b_dev, b_host.get(), bytes, cudaMemcpyDefault));
-    float h2d_ms = cpu_timer.stop();
-
-    // Run CPU benchmark
-    int repeat_cpu = 3;
-    auto time_stats_cpu = benchmark_cpu([&]() {
-        vector_add_cpu(a_host.get(), b_host.get(), result_host.get(), n);
-    },
-    repeat_cpu, cpu_timer);
-
-    // Run GPU benchmark
-    int repeat_gpu = 10;
-    auto time_stats_kernel = benchmark_cuda_kernel([&]() {
-        // Launch the kernel
-        launch_vector_add(a_dev, b_dev, result_dev, n);
-    },
-    repeat_gpu, cuda_timer);
-
-    // Wait kernel to finish execution
-    gpu::cuda_check(cudaDeviceSynchronize());
-
-    cpu_timer.start();
-    // Copy kernel results back to host
-    gpu::cuda_check(cudaMemcpy(result_from_dev.get(), result_dev, bytes, cudaMemcpyDefault));
-    float d2h_ms = cpu_timer.stop();
-
-    if(check_result(result_host.get(), result_from_dev.get(), n))
+    // Create directory to save benchmark results csv file
+    std::error_code ec;
+    std::filesystem::create_directories("results/data");
+    if (ec)
     {
-        fmt::print("Vector add: CPU and GPU results match\n");
-    }
-    else
-    {
-        fmt::print("Vector add: Error - CPU and GPU results do not match\n");
+        fmt::print("{}\n", ec.message());
     }
 
-    // Effective bandwidth
-    // GPU: read a + read b + write c = 3 * n * sizeof(float);
-    size_t gpu_bytes = 3 * bytes;
-    double gpu_bandwidth_GB_s = bandwidth_GB_s(gpu_bytes, time_stats_kernel.avg_ms);
-    double gpu_gflops = calculate_gflops(n, time_stats_kernel.avg_ms);
+    std::ofstream out("results/data/vector_add.csv");
+    write_csv_header(out);
 
-    // CPU: assume that arrays are large enough that they can not be put into the last-level cache
-    // thus the DRAM bandwidth is measured (roughly): read a + read b + write c = 3 * n * sizeof(float);
-    size_t cpu_bytes = 3 * bytes;
-    double cpu_bandwidth_GB_s = bandwidth_GB_s(cpu_bytes, time_stats_cpu.avg_ms);
-    double cpu_gflops = calculate_gflops(n, time_stats_cpu.avg_ms);
+    // Vector size to sweep
+    std::vector<size_t> sizes_vec{1 << 18, 1 << 19, 1 << 20, 1 << 21, 1 << 22,
+                                  1 << 23, 1 << 24, 1 << 25, 1 << 26, 1 << 27};
 
-    // H2D bandwidth
-    size_t h2d_bytes = 2 * bytes;
-    double h2d_bandwidth_GB_s = bandwidth_GB_s(h2d_bytes, h2d_ms);
+    // GPU block size to sweep
+    std::vector<int> block_sizes{64, 128, 256, 512, 1024};
 
-    // D2H bandwidth
-    size_t d2h_bytes = 1 * bytes;
-    double d2h_bandwidth_GB_s = bandwidth_GB_s(d2h_bytes, d2h_ms);
+    // Number of measurement repetitions
+    int repeat_cpu  = 5;
+    int repeat_gpu  = 50;
+    int repeat_copy = 5;
 
-    // End-to-end effective bandwidth
-    size_t e2e_bytes = 3 * bytes; // H2D two arrays + D2H one array
-    double e2e_ms = h2d_ms + time_stats_kernel.avg_ms + d2h_ms;
-    double e2e_bandwidth_GB_s = bandwidth_GB_s(e2e_bytes, e2e_ms);
+    // Sweep over vector lengths
+    for (size_t n : sizes_vec)
+    {
+        size_t bytes = bytes_per_vector<float>(n);
+        fmt::print("Running vector_add benchmark for n = {} (Array size = {} MiB)\n",
+                   n, static_cast<double>(bytes) / static_cast<double>(1ULL << 20));
 
-    fmt::print("Average cpu serial time: {} ms\n", time_stats_cpu.avg_ms);
-    fmt::print("H2D copy time: {} ms\n", h2d_ms);
-    fmt::print("Average kernel-only time: {} ms\n", time_stats_kernel.avg_ms);
-    fmt::print("D2H copy time: {} ms\n", d2h_ms);
-    fmt::print("GPU End-to-end time: {} ms\n", e2e_ms);
-    fmt::print("Effevtive CPU bandwidth = {} GB/s\n", cpu_bandwidth_GB_s);
-    fmt::print("Effevtive GPU bandwidth = {} GB/s\n", gpu_bandwidth_GB_s);
-    fmt::print("H2D bandwidth: {} GB/s\n", h2d_bandwidth_GB_s);
-    fmt::print("D2H bandwidth: {} GB/s\n", d2h_bandwidth_GB_s);
-    fmt::print("GPU End-to-end effective bandwidth: {} GB/s\n", e2e_bandwidth_GB_s);
-    fmt::print("CPU GFlop/s: {}\n", cpu_gflops);
-    fmt::print("GPU GFlop/s: {}\n", gpu_gflops);
+        // Pointers to host memory
+        host_ptr a_host(cuda_malloc_host<float>(n));
+        host_ptr b_host(cuda_malloc_host<float>(n));
+        host_ptr result_host(cuda_malloc_host<float>(n));
+        host_ptr result_from_dev(cuda_malloc_host<float>(n));
 
+        // Pointers to device memory
+        float* a_dev = nullptr;
+        float* b_dev = nullptr;
+        float* result_dev = nullptr;
 
-    gpu::cuda_check(cudaFree(a_dev));
-    gpu::cuda_check(cudaFree(b_dev));
-    gpu::cuda_check(cudaFree(result_dev));
+        // Initialize vectors on the host
+        init_array(a_host.get(), n);
+        init_array(b_host.get(), n);
+
+        // CPU benchmark
+        auto time_stats_cpu = benchmark_cpu(
+            [&]() {
+                vector_add_cpu(a_host.get(), b_host.get(), result_host.get(), n);
+            },
+        repeat_cpu, cpu_timer);
+
+        // Write CPU benchmark results
+        write_csv_row(out,
+            "vector_add",
+            "cpu_serial",
+            "cpu",
+            "fp32",
+            n,
+            0,
+            repeat_cpu,
+            time_stats_cpu,
+            bytes_vector_add(n),
+            true);
+
+        // Allocate device memory
+        gpu::cuda_check(cudaMalloc(&a_dev, bytes));
+        gpu::cuda_check(cudaMalloc(&b_dev, bytes));
+        gpu::cuda_check(cudaMalloc(&result_dev, bytes));
+
+        // H2D benchmark
+        auto time_stats_h2d = benchmark_cpu(
+            [&]() {
+                // Copy data to the GPU
+                gpu::cuda_check(cudaMemcpy(a_dev, a_host.get(), bytes, cudaMemcpyDefault));
+                gpu::cuda_check(cudaMemcpy(b_dev, b_host.get(), bytes, cudaMemcpyDefault));
+            },
+        repeat_copy, cpu_timer);
+
+        // Write H2D benchmark results
+        write_csv_row(out,
+            "vector_add",
+            "pageable_memcpy",
+            "h2d",
+            "fp32",
+            n,
+            0,
+            repeat_copy,
+            time_stats_h2d,
+            h2d_bytes_vector_add(n),
+            true);
+
+        // CUDA kernel sweep over block sizes
+        for (size_t block_size : block_sizes)
+        {
+            auto time_stats_kernel = benchmark_cuda_kernel([&]() {
+                // Launch the kernel
+                launch_vector_add(a_dev, b_dev, result_dev, n, block_size);
+            },
+            repeat_gpu, cuda_timer);
+
+            // Wait kernel to finish execution
+            gpu::cuda_check(cudaDeviceSynchronize());
+
+            // Copy kernel results back to host
+            gpu::cuda_check(cudaMemcpy(result_from_dev.get(), result_dev, bytes, cudaMemcpyDefault));
+
+            // check results correctness
+            bool correct = check_result(result_host.get(), result_from_dev.get(), n);
+
+            // Write CPU benchmark results
+            write_csv_row(out,
+                "vector_add",
+                "cuda_naive",
+                "cuda_kernel",
+                "fp32",
+                n,
+                block_size,
+                repeat_gpu,
+                time_stats_kernel,
+                bytes_vector_add(n),
+                correct);
+        }
+
+        // D2H benchmark
+        auto time_stats_d2h = benchmark_cpu(
+            [&]() {
+                // Copy kernel results back to host
+                gpu::cuda_check(cudaMemcpy(result_from_dev.get(), result_dev,
+                                           bytes, cudaMemcpyDefault));
+            },
+        repeat_copy, cpu_timer);
+
+        // Write D2H benchmark results
+        write_csv_row(out,
+            "vector_add",
+            "pageable_memcpy",
+            "d2h",
+            "fp32",
+            n,
+            0,
+            repeat_copy,
+            time_stats_d2h,
+            d2h_bytes_vector_add(n),
+            true);
+
+        gpu::cuda_check(cudaFree(a_dev));
+        gpu::cuda_check(cudaFree(b_dev));
+        gpu::cuda_check(cudaFree(result_dev));
+    }
 
     return 0;
 }
