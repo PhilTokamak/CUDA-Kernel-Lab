9edc05ca61d030ec1b62adc2dc6058a7645bdbc6
Phil Tokamak <phil.tokamak@icloud.com>
2026-05-16 21:55:50 +0200

    small fix

--- benchmarks/bench_vector_add.cu
+++ benchmarks/bench_vector_add.cu
@@ -91,26 +91,26 @@ int main()
 
     // Effective bandwidth
     // GPU: read a + read b + write c = 3 * n * sizeof(float);
-    double gpu_bytes = 3.0 * static_cast<double>(bytes);
+    size_t gpu_bytes = 3 * bytes;
     double gpu_bandwidth_GB_s = bandwidt_GB_s(gpu_bytes, time_stats_kernel.avg_ms);
     double gpu_gflops = gflops(n, time_stats_kernel.avg_ms);
 
     // CPU: assume that arrays are large enough that they can not be put into the last-level cache
     // thus the DRAM bandwidth is measured (roughly): read a + read b + write c = 3 * n * sizeof(float);
-    double cpu_bytes = 3.0 * static_cast<double>(bytes);
+    size_t cpu_bytes = 3 * bytes;
     double cpu_bandwidth_GB_s = bandwidt_GB_s(cpu_bytes, time_stats_cpu.avg_ms);
     double cpu_gflops = gflops(n, time_stats_cpu.avg_ms);
 
     // H2D bandwidth
-    double h2d_bytes = 2.0 * static_cast<double>(bytes);
+    size_t h2d_bytes = 2 * bytes;
     double h2d_bandwidth_GB_s = bandwidt_GB_s(h2d_bytes, h2d_ms);
 
     // D2H bandwidth
-    double d2h_bytes = 1.0 * static_cast<double>(bytes);
+    size_t d2h_bytes = 1 * bytes;
     double d2h_bandwidth_GB_s = bandwidt_GB_s(d2h_bytes, d2h_ms);
 
     // End-to-end effective bandwidth
-    double e2e_bytes = 3.0 * static_cast<double>(bytes); // H2D two arrays + D2H one array
+    size_t e2e_bytes = 3 * bytes; // H2D two arrays + D2H one array
     double e2e_ms = h2d_ms + time_stats_kernel.avg_ms + d2h_ms;
     double e2e_bandwidth_GB_s = bandwidt_GB_s(e2e_bytes, e2e_ms);
 
